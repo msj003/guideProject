2ec2fa7b130172f1e6c1edc597f4e3c8d91b93c0
Manjinder Singh <msj003@gmail.com>
2018-02-21 09:52:54 -0500

    Working wiht username

--- src/main/java/me/manjinder/project/guideProj/database/ConfigData.java
+++ src/main/java/me/manjinder/project/guideProj/database/ConfigData.java
@@ -2,7 +2,7 @@ package me.manjinder.project.guideProj.database;
 
 //  @ Manjinder Singh - manjindersingh.me
 public class ConfigData {
-	public static String JDBC_URL = "jdbc:mysql://35.203.73.140:3306/testApp";
-	public static String JDBC_USER = "root";
-	public static String JDBC_PASS = "testing";
+	public static String JDBC_URL = "serverip/database";
+	public static String JDBC_USER = "user";
+	public static String JDBC_PASS = "password";
 }
