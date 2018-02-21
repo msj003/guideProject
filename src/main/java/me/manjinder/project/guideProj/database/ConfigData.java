b3de5a2e88f38deec5837b4c8634c9e1b9cad3b3
Manjinder Singh <msj003@gmail.com>
2018-02-20 19:29:10 -0500

    git ignore

--- src/main/java/me/manjinder/project/guideProj/database/ConfigData.java
+++ src/main/java/me/manjinder/project/guideProj/database/ConfigData.java
@@ -1,6 +1,6 @@
 package me.manjinder.project.guideProj.database;
 
-//@ Manjinder Singh - manjindersingh.me
+//  @ Manjinder Singh - manjindersingh.me
 public class ConfigData {
 	public static String JDBC_URL = "jdbc:mysql://35.203.73.140:3306/testApp";
 	public static String JDBC_USER = "root";
